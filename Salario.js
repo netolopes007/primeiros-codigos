8c203bb073be401df76681f9acee48e0289f083d
netolopes007 <netovictor.007@gmail.com>
2023-08-30 19:11:25 -0400

    salario.js

--- Salario.js
+++ Salario.js
@@ -1,3 +1,18 @@
-var salario=prompt ("digite o salário");
-var cargaH=prompt ("digite sua carga Horaria");
-alert (salario/16)
+var salario = 0;
+var salarioliquido = 0;
+var salarioBase = parseFloat(prompt("Quanto é seu salário base? "));  
+var horaSemanalTrabalhada = parseFloat(prompt("Quantas horas semanais trabalha? "));
+var hora = salarioBase/160 // 40h da carga trabalhada semanal * 4 semanas do mês aproximada
+var horaExtra = hora*1.5;
+ if(horaSemanal = Trabalhada >= 40){
+salario= salarioBase +(horaExtra *(horaSemanalTrabalhada-40));
+salarioliquido = salario * 0.975; // ISS calculado é 2,5% alert("Salário bruto é de R$"+ salario.toFixed(2));
+alert(" Salário líquido é de R$" + salarioLiquido.toFixed(2));
+ }
+else {
+salario salarioBase - (horaSemanal Trabalhada*hora);
+salarioliquido salario * 0.975
+}
+; alert("Salário bruto é de R$"+ salario.toFixed(2));
+
+alert(" Salário líquido é de R$" + salarioliquido.toFixed(2));
